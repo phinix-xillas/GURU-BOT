ff9f0893dd06148321877b02662ffcb4c54f0e4d
phinix-xillas <139241502+phinix-xillas@users.noreply.github.com>
2023-12-23 14:27:59 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254798758272', 'phinix bot', true],
+  ['6281255369012', 'Ash, true], 
   [''] 
 ] //Number of owners
 
